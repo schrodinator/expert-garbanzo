4dc1433be3aa167617de6e908fb0acd99f9317a9
Schrodinator <schrodinator@gmail.com>
2023-11-08 11:37:59 -0800

    combine tests for more idiomatic Go

--- game_test.go
+++ game_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// Calling Change() on a Role should return a Role.
+// Change Role from cluegiver to guesser to cluegiver
 func TestRoleChangeType(t *testing.T) {
 	var myRole Role
 	myRole = cluegiver
@@ -13,81 +13,55 @@ func TestRoleChangeType(t *testing.T) {
 	if reflect.TypeOf(roleChange) != reflect.TypeOf(myRole) {
 		t.Errorf("type of roleChange: %v", reflect.TypeOf(roleChange))
 	}
-}
-
-/* Calling Change() on a cluegiver Role should return
-   the guesser Role. */
-func TestRoleChangeValue0(t *testing.T) {
-	myRole := cluegiver
-	roleChange := myRole.Change()
 	if roleChange != guesser {
-		t.Errorf("value of roleChange: %v", roleChange)
+		t.Errorf("value of first roleChange: %v", roleChange)
 	}
-}
 
-/* Calling Change() on a guesser Role should return
-   the cluegiver Role. */
-func TestRoleChangeValue1(t *testing.T) {
-	myRole := guesser
-	roleChange := myRole.Change()
+	roleChange = roleChange.Change()
 	if roleChange != cluegiver {
-		t.Errorf("value of roleChange: %v", roleChange)
+		t.Errorf("value of second roleChange: %v", roleChange)
 	}
 }
 
-// Calling Change() on a Team should return a Team.
-func TestTeamChangeType(t *testing.T) {
+// Change Team from red to blue to red
+func TestTeamChange(t *testing.T) {
 	var myTeam Team
 	myTeam = red
+
 	teamChange := myTeam.Change()
 	if reflect.TypeOf(teamChange) != reflect.TypeOf(myTeam) {
-		t.Errorf("type of teamChange is %v", reflect.TypeOf(teamChange))
+		t.Errorf("type of first teamChange is %v", reflect.TypeOf(teamChange))
 	}
-}
-
-// Calling Change() on the red Team should return blue.
-func TestTeamChangeValue0(t *testing.T) {
-	myTeam := red
-	teamChange := myTeam.Change()
 	if teamChange != blue {
-		t.Errorf("value of teamChange is %v", teamChange)
+		t.Errorf("value of first teamChange is %v", teamChange)
 	}
-}
 
-// Calling Change() on the blue Team should return red.
-func TestTeamChangeValue1(t *testing.T) {
-	myTeam := blue
-	teamChange := myTeam.Change()
+	teamChange = teamChange.Change()
 	if teamChange != red {
-		t.Errorf("value of teamChange is %v", teamChange)
+		t.Errorf("value of second teamChange is %v", teamChange)
 	}
 }
 
-// Calling changeTurn() with red cluegiver should set red guesser.
-func TestChangeTurnRedCluegiver(t *testing.T) {
+// Turns should go (red cluegiver) -> (red guesser) -> (blue cluegiver)
+func TestChangeTurn(t *testing.T) {
 	var game Game
 	game.roleTurn = cluegiver
 	game.teamTurn = red
+
 	game.teamTurn, game.roleTurn = changeTurn(game.teamTurn, game.roleTurn)
 	if game.roleTurn != guesser {
-		t.Errorf("role after changeTurn is %v", game.roleTurn)
+		t.Errorf("role after first changeTurn is %v", game.roleTurn)
 	}
 	if game.teamTurn != red {
-		t.Errorf("team after changeTurn is %v", game.teamTurn)
+		t.Errorf("team after first changeTurn is %v", game.teamTurn)
 	}
-}
 
-// Calling changeTurn() with red guesser should set blue cluegiver.
-func TestChangeTurnRedGuesser(t *testing.T) {
-	var game Game
-	game.roleTurn = guesser
-	game.teamTurn = red
 	game.teamTurn, game.roleTurn = changeTurn(game.teamTurn, game.roleTurn)
 	if game.roleTurn != cluegiver {
-		t.Errorf("role after changeTurn is %v", game.roleTurn)
+		t.Errorf("role after second changeTurn is %v", game.roleTurn)
 	}
 	if game.teamTurn != blue {
-		t.Errorf("team after changeTurn is %v", game.teamTurn)
+		t.Errorf("team after second changeTurn is %v", game.teamTurn)
 	}
 }
 
@@ -111,22 +85,23 @@ func TestUpdateScore(t *testing.T) {
 	}
 
 	type test struct {
+		name        string
 		cardColor   string
 		expectScore Score
 	}
 	tests := []test{
-		{ cardColor: "red", expectScore: Score{red: 8, blue: 8} },
-		{ cardColor: "blue", expectScore: Score{red: 8, blue: 7} },
-		{ cardColor: "neutral", expectScore: Score{red: 8, blue: 7} },
-		{ cardColor: deathCard, expectScore: Score{red: 8, blue: 7} },
-		{ cardColor: "red", expectScore: Score{red: 7, blue: 7} },
+		{ name: "red", cardColor: "red", expectScore: Score{red: 8, blue: 8} },
+		{ name: "blue", cardColor: "blue", expectScore: Score{red: 8, blue: 7} },
+		{ name: "neutral", cardColor: "neutral", expectScore: Score{red: 8, blue: 7} },
+		{ name: "death card", cardColor: deathCard, expectScore: Score{red: 8, blue: 7} },
+		{ name: "second red", cardColor: "red", expectScore: Score{red: 7, blue: 7} },
 	}
 
 	for _, tt := range tests {
 		game.updateScore(tt.cardColor)
 		if !reflect.DeepEqual(game.score, tt.expectScore) {
 			t.Fatalf("test %v: expected: %v, got: %v",
-			         tt.cardColor, tt.expectScore, game.score)
+			         tt.name, tt.expectScore, game.score)
 		}
 	}
 }
